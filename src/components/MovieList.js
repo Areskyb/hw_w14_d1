314d0bef9ecbb470e245a61c9726252529c72acc
Aresky Berkane <areskyberkane@gmail.com>
2020-01-27 23:49:03 +0000

    added method for the list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import Movie from './Movie';
 
 class MovieList extends Component{
 
+render(){
 
+    const movies = this.props.data.map(movie => {
+        return(
+            <>
+            <Movie data={movie.url} key={movie.id}>{movie.name}</Movie>
+            </>
+            )
+    })
 
+    return(
+        <ul>
+            {movies}
+        </ul>
+    )
+}
 
     
 }
